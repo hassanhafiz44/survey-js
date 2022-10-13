11b8dd04f391eb10921d5cf4a31dc82e9c3d9dda
Muhammad Hassan Javed <hassanhafiz44@gmail.com>
2022-10-13 20:11:55 +0500

    Adding questions completed.

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ document.addEventListener("DOMContentLoaded", () => {
   fetch("./survey.json")
     .then((response) => response.json())
     .then((data) => {
-      createForm(data, "questionnaire");
+      const survey = new Survey(data, "questionnaire");
+      // survey.createSurvey();
+      survey.editSurvey();
     });
 });
